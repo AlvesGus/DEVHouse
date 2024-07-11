f14bb21a8dd94f1bef24de8020f229345473f518
gualves <gustavo.allvess@icloud.com>
2024-07-11 14:50:27 -0300

    feat: adicioando validações de envio de informações

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -8,19 +8,28 @@
   destroy: deletar uma sessao
 */
 import User from '../models/User'
+import * as Yup from 'yup'
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required()
+    })
+
     const { email } = req.body
 
     // let user = await User.create({ email })
 
     let user = await User.findOne({ email })
 
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Favor digitar um email valido' })
+    }
+
     if (!user) {
       await User.create({ email })
     }
-    
+
     return res.json(user)
   }
 }
